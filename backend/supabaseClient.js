30cd04d813d2e742ae90ae8e0db242d859c7eec9
Yashrxx <yashkumar.s2023@vitstudent.ac.in>
2025-07-02 23:19:40 +0530

    new changes

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = 'https://prqpioqtpsjqzdmxdnrl.supabase.co';
-const supabaseKey = process.env.anon;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
 
 module.exports = supabase;
